4bef88364aba85df1701ec4da946bd1fd619080d
Kurosity <40587453+Kurosity@users.noreply.github.com>
2019-02-05 23:03:51 -0500

    Update App.Js
    Added code getting worked on currently

--- src/App.js
+++ src/App.js
@@ -1,119 +1,86 @@
-import React, { Component } from 'react';
-import {GoogleApiWrapper} from 'google-maps-react'
+import React, { Component } from 'react'
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import './App.css';
-import Container from './Component/Container'
 import markets from './data/markets'
-import Drawer from './Component/MDrawer'
+import Container from './Components/Container'
 
-class App extends Component {
-  state = {
-    lat: 36.8529,
-    lng: -75.9780, 
-    zoom: 11.5,
-    locations: markets,
-    openDrawer: false,
-    fLocations: null
-  }
+// ...
 
-    menuSelect = (mName) => {
-    let {infoWindow} = this.state
-    let mSData = this.locations.filter(market => 
-     market.name.toLowerCase().includes(mName.toLowerCase()))
-    
-    this.populateInfoWindow=(mSData, infoWindow)
-   } 
+export class App extends Component {
 
-    populateInfoWindow = (marker, infoWindow) => {
-      // console.log(marker.title)
-      if(this.state.marker !== marker){
-        this.setState(infoWindow.marker = marker)
-          this.state.setContent(`
-          <div>   
-              <h3>${marker.title}</h3>
-          </div>
-          <div>
-              <ul style='list-style-type:none'>
-                  <li>${marker.address}</li>
-                  <li>${marker.city}</li>
-                  <li><a href='${marker.link}'>More Information</a></li>
-              </ul>
-          </div>
-          `)
-          infoWindow.open(this.map, marker)
-          infoWindow.addListener('closeclick', function(){
-              infoWindow.marker = null
-          })
+    state = {
+        lat: 36.8529,
+        lng: -75.9780, 
+        zoom: 12,
+        locations: markets,
+        openDrawer: false,
+        fLocations: markets,
+        // infoWindow: new this.props.google.maps.InfoWindow(),
+        // markers: []
+        isOpen: false
       }
+    
+    //Styles for map and the menu button
+    styles = {
+        button:{
+            marginLeft: 10,
+            marginRight: 20,
+            position: "absolute",
+            left: 10,
+            top: 20,
+            background: 'black',
+            padding: 10
+        },
+
+        map:{
+            width: '100vw',
+            height: '100vh',
+            position: 'relative',
+        }
     }
 
-  componentDidMount = () => {
-    // this.setState({
-    //   ...this.state,
-    //   fLocations: this.state.locations
-    // })
-    console.log(this.state.fLocations)
-  }
-
-  filterMarket = (locations, query) => {
-    return locations.filter(market => 
-      market.name.toLowerCase().includes(query.toLowerCase()))
-  }
-
-  updateMarketQuery = (query) => {
-    this.setState({
-      ...this.state,
-      selectedIndex: null,
-      fLocations: this.filterMarket(this.state.locations, query)
-    })
-  }
-
-  toggle = () => {
-    this.setState({ openDrawer: !this.state.openDrawer})
-  }
-
-  styles = {
-    button:{
-      marginLeft: 10,
-      marginRight: 20,
-      position: "absolute",
-      left: 10,
-      top: 20,
-      background: 'black',
-      padding: 10
+    //Toggle menu open and close
+    toggle = () => {
+        this.setState({ openDrawer: !this.state.openDrawer})
+      }
+    
+    //Toggle the InfoWindow
+    toggleInfoWindow = () => {
+        this.setstate({
+            isOpen: !this.state.isOpen
+        })
     }
-  }
+    
+    clickMarker = () => {
 
-  render() {
-    console.log(this.state.fLocations)
-    return (
-      <div className="App">
+    }
+
+    render() {
+        return (
+        <div className="App" role='application'>
         <div>
-          <button onClick={this.toggle} style={this.styles.button}>
+            <button onClick={this.toggle} style={this.styles.button}>
             <i className='menuButton'></i>
-          </button>
-          <p>
+            </button>
+            <p>
             My Neighborhod Map
-          </p>
+            </p>
         </div>
-        <Container 
-          lat={this.state.lat}
-          lng={this.state.lng}
-          zoom={this.state.zoom}
-          locations={this.state.locations}
-          fLocation={this.state.fLocations}
-          google={this.props.google}/>
-        <Drawer
-          locations={this.state.fLocations}
-          open={this.state.openDrawer}
-          toggle={this.toggle}
-          fLocations={this.updateMarketQuery}
-        />
-      </div>
-    );
-  }
+
+            <Container 
+                lat={this.state.lat}
+                lng={this.state.lng}
+                zoom={this.state.zoom}
+                locations={this.state.locations}
+                fLocations={this.state.fLocations}
+                google={this.props.google}
+                style={this.styles.map}
+                open={this.state.isOpen}
+                toggleWindow={this.toggleInfoWindow}
+            />
+          </div>
+        );
+      }
 }
 
- //  export default App
-  export default GoogleApiWrapper({
-    apiKey:'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
-  })(App)
+export default App
