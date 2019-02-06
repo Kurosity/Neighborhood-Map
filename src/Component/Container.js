e8ddd2c11ddc260f47ebde9cadcd40bc606eab6a
Kurosity <40587453+Kurosity@users.noreply.github.com>
2019-02-05 23:07:52 -0500

    Update Container.js
    Added code getting worked on currently

--- src/Component/Container.js
+++ src/Component/Container.js
@@ -1,178 +1,19 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-// import {Map, InfoWindow} from 'google-maps-react'
+import React, { Component } from 'react';
+import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react'
 
-
-class Container extends Component{
+class Container extends Component {
     state = {
-        google: this.props.google,
-        // locations: this.props.locations,
-        fMarkets: this.props.fLocation,
-        markers: [],
-        infoWindow: new this.props.google.maps.InfoWindow()
-    }
-    
-    componentDidUpdate = () =>{
-        // console.log(this.props.fLocation)
-        this.loadMap()
+        locations: this.props.locations,
+        mStyle: this.props.style,
+        zoom: this.props.zoom,
+        lat: this.props.lat,
+        lng: this.props.lng,
+        open: this.props.open,
+        toggle: this.props.toggleWindow    
     }
 
-    // fMarkets = this.props.fLocation
-
-    loadMap(){
-        // console.log(this.props.fLocation)
-        console.log(this.props.fLocation)
-        // console.log(this.state.fMarkets)
-        if(this.props && this.props.google){
-            let {google} = this.props
-            let maps = google.maps
-            let mapRef = ReactDOM.findDOMNode(this.refs.map)
-
-            let mConfig = Object.assign({}, {
-                center: {       
-                    lat: this.props.lat,
-                    lng: this.props.lng 
-                },
-                zoom: this.props.zoom,
-                mTypeID: 'mMap'
-            })
-
-            this.map = new maps.Map(mapRef, mConfig)
-            this.addMarkers()
-        }
-    }
-
-    componentDidMount(){
-        this.loadMap()
-    }
-
-    
-    // updateMarkers = (market) => {
-    //     if(!market){
-    //         return
-    //     }
-
-    //     this.state.markers.forEach(marker => marker.setMap(null))
-
-
-    // }
-
-    addMarkers =() => {
-        let {google} = this.props
-        let mbounds = new google.maps.LatLngBounds()
-        let {infoWindow} = this.state
-        let mLocations = this.props.fLocation
-        // let mFLocations = this.fMarkets
-        let animation = this.props.google.maps.Animation.DROP
-        // let mLocations = null
-        // console.log(this.fMarkets)
-console.log(mLocations)
-        // if (this.fMarkets === null){
-        //     mLocations = this.state.locations
-        // }
-        // else{
-        //     mLocations = this.fMarkets
-        // }
-
-        mLocations.forEach( location => {
-            let mMarker = new google.maps.Marker({
-                position: {
-                    lat: location.pos.lat, 
-                    lng: location.pos.lng},
-                map: this.map,
-                title: location.name,
-                address: location.street,
-                city: location.city,
-                link: location.url,
-                animation
-                
-            })
-console.log(location.title)
-            mMarker.addListener('click', ()=>{
-                this.populateInfoWindow(mMarker, infoWindow)
-                })
-            // this.setState((state) => ({
-            //     markers:[state.markers, mMarker]
-            // }))
-            mbounds.extend(mMarker.position)
-        })
-    
-
-        // if(mLocations === null || 
-        //     mLocations === undefined || 
-        //     mLocations === ''
-        //     ){           
-        //     this.props.locations.forEach( location => {
-        //         let mMarker = new google.maps.Marker({
-        //             position: {
-        //                 lat: location.pos.lat, 
-        //                 lng: location.pos.lng},
-        //             map: this.map,
-        //             title: location.name,
-        //             address: location.street,
-        //             city: location.city,
-        //             link: location.url,
-        //             animation
-        //         })
-
-        //         mMarker.addListener('click', ()=>{
-        //             this.populateInfoWindow(mMarker, infoWindow)
-        //             })
-        //         // this.setState((state) => ({
-        //         //     markers:[state.markers, mMarker]
-        //         // }))
-        //         mbounds.extend(mMarker.position)
-        //     })
-        // }
-        // else{
-        //     mLocations.forEach( location => {
-        //         let mMarker = new google.maps.Marker({
-        //             position: {
-        //                 lat: location.pos.lat, 
-        //                 lng: location.pos.lng},
-        //             map: this.map,
-        //             title: mLocations.name,
-        //             address: mLocations.street,
-        //             city: mLocations.city,
-        //             link: mLocations.url,
-        //             animation                
-        //         })
-
-        //         mMarker.addListener('click', ()=>{
-        //             this.populateInfoWindow(mMarker, infoWindow)
-        //             })
-        //         // this.setState((state) => ({
-        //         //     markers:[state.markers, mMarker]
-        //         // }))
-        //         mbounds.extend(mMarker.position)
-        //     })
-        // }
-        
-        this.map.fitBounds(mbounds)
-    }
-
-    populateInfoWindow = (marker, infoWindow) => {
-        // console.log(marker.title)
-        if(infoWindow.marker !== marker){
-            this.setState(infoWindow.marker = marker)
-            infoWindow.setContent(`
-            <div>   
-                <h3>${marker.title}</h3>
-            </div>
-            <div>
-                <ul style='list-style-type:none'>
-                    <li>${marker.address}</li>
-                    <li>${marker.city}</li>
-                    <li><a href='${marker.link}'>More Information</a></li>
-                </ul>
-            </div>
-            `)
-            infoWindow.open(this.map, marker)
-            infoWindow.addListener('closeclick', function(){
-                infoWindow.marker = null
-            })
-        }
-    }
+    fLocations = this.props.fLocations
+    // toggle = this.props.toggleWindow
 
     style = {
         width: '100vw',
@@ -181,21 +22,60 @@ console.log(location.title)
     }
 
     render(){
-        // let {markers} = this.state
-        //  console.log(this.props.mLocations)
-        // console.log(this.state.markers)
+        console.log(this.state.open)
+        let mMarkets
+        if(this.fLocations === null){
+            mMarkets = this.state.locations
+        }
+        else{
+            mMarkets = this.fLocations
+        }
+
         return(
-        <div>
-            <div 
-                role='application' 
-                style={this.style} 
-                className='marMap' 
-                ref='map'>
-            </div>
-        </div>
+            <Map 
+                google={this.props.google} 
+                zoom={this.state.zoom}
+                initialCenter={{
+                    lat: this.state.lat,
+                    lng: this.state.lng
+                    }}
+                aria-label='map'
+                style={this.state.mStyle}
+            >
+            {mMarkets.map((market) => {
+                return(
+                    <Marker
+                        key={market.name}
+                        name={market.name}
+                        position={market.pos} 
+                        animation={this.props.google.maps.Animation.DROP}
+                        onClick={this.state.toggleWindow}
+                     >
+                     {this.state.open &&
+                        (<InfoWindow
+                            onCloseClick={this.props.toggleWindow}
+                        >
+                            <div>   
+                                <h3>${market.name}</h3>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>${market.street}</li>
+                                    <li>${market.city}</li>
+                                    <li><a href="market.link">More Information</a></li> //fix href link
+                                </ul>
+                            </div>
+                        </InfoWindow>)
+                     }
+                     </Marker>   
+                )
+            })}
+
+            </Map> 
         )
     }
 }
 
-
-export default Container
+export default GoogleApiWrapper({
+    apiKey:'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+})(Container)
