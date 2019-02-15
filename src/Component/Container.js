39a7b726fcb81344a70e9c4ef93d86873b97e5cf
Kurosity <40587453+Kurosity@users.noreply.github.com>
2019-02-14 23:45:46 -0500

    Update Container.js
    Updated to use Foursquare data

--- src/Component/Container.js
+++ src/Component/Container.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-// import {Map, InfoWindow} from 'google-maps-react'
 
 
 class Container extends Component{
@@ -9,7 +8,8 @@ class Container extends Component{
         locations: this.props.locations,
         fMarkets: this.props.fLocation,
         markers: [],
-        infoWindow: new this.props.google.maps.InfoWindow()
+        infoWindow: new this.props.google.maps.InfoWindow(),
+        mMarket: this.props.mMarket
     }
    
     /*
@@ -17,8 +17,6 @@ class Container extends Component{
     * Reset the marker
     */
     componentDidUpdate = = (prevProps) =>{
-        console.log(this.state.fMarkets)
-        console.log(prevProps.fMarkets)
         if(this.props.fLocation !== prevProps.fLocation){
 
             this.setState((state) => ({
@@ -33,9 +31,7 @@ class Container extends Component{
 
     //Once the map loads all the markers using the configuration in mConfig
     loadMap(){
-        // console.log(this.props.fLocation)
-        // console.log(this.props.fLocation)
-        // console.log(this.state.fMarkets)
+
         if(this.props && this.props.google){
             let {google} = this.props
             let maps = google.maps
@@ -68,131 +64,72 @@ class Container extends Component{
     addMarkers =() => {
         let {google} = this.props
         let mbounds = new google.maps.LatLngBounds()
-        let {infoWindow} = this.state
+        let infoWindow = this.state.infoWindow
         let mLocations = this.props.fLocation
         let animation = this.props.google.maps.Animation.DROP
-
-//         if (this.fMarkets === null){
-//             mLocations = this.state.locations
-//         }
-//         else{
-//             mLocations = this.fMarkets
-//         }
+        let testMark = []
 
         mLocations.forEach( location => {
             let mMarker = new google.maps.Marker({
                 position: {
-                    lat: location.pos.lat, 
-                    lng: location.pos.lng},
+                    lat: location.venue.location.lat, 
+                    lng: location.venue.location.lng},
                 map: this.map,
-                title: location.name,
-                address: location.street,
-                city: location.city,
-                link: location.url,
+                title: location.venue.name,
+                category: location.venue.categories[0].name,
+                city: location.venue.location.city,
                 animation
-                
             })
-// console.log(location.title)
+
             mMarker.addListener('click', ()=>{
                 this.populateInfoWindow(mMarker, infoWindow)
                 })
             
-//                 this.setState((state) => ({
-//                     markers: state.markers.concat(mMarker)
-//                 }))
+            this.setState((state) => ({                
+                markers: state.markers.concat(mMarker)                
+            }))
+
+            testMark.push(mMarker)
             
             mbounds.extend(mMarker.position)
-        })
-    
-
-        /*
-        * Display markers based on mLocations 
-        * (compare if mLocations not populated/null/undefined, use unfiltered locations (locations))
-        * else use the filtered locations (mLocations)
-        *
-        * This is possible alternative for the above code
-        */
-        
-        //if(mLocations === null || 
-        //     mLocations === undefined || 
-        //     mLocations === ''
-        //     ){           
-        //     this.props.locations.forEach( location => {
-        //         let mMarker = new google.maps.Marker({
-        //             position: {
-        //                 lat: location.pos.lat, 
-        //                 lng: location.pos.lng},
-        //             map: this.map,
-        //             title: location.name,
-        //             address: location.street,
-        //             city: location.city,
-        //             link: location.url,
-        //             animation
-        //         })
-
-        //         mMarker.addListener('click', ()=>{
-        //             this.populateInfoWindow(mMarker, infoWindow)
-        //             })
-        //         // this.setState((state) => ({
-        //         //     markers:[state.markers, mMarker]
-        //         // }))
-        //         mbounds.extend(mMarker.position)
-        //     })
-        // }
-        // else{
-        //     mLocations.forEach( location => {
-        //         let mMarker = new google.maps.Marker({
-        //             position: {
-        //                 lat: location.pos.lat, 
-        //                 lng: location.pos.lng},
-        //             map: this.map,
-        //             title: mLocations.name,
-        //             address: mLocations.street,
-        //             city: mLocations.city,
-        //             link: mLocations.url,
-        //             animation                
-        //         })
-
-        //         mMarker.addListener('click', ()=>{
-        //             this.populateInfoWindow(mMarker, infoWindow)
-        //             })
-        //         // this.setState((state) => ({
-        //         //     markers:[state.markers, mMarker]
-        //         // }))
-        //         mbounds.extend(mMarker.position)
-        //     })
-        // }
-        
+        })   
+   
         this.map.fitBounds(mbounds)
+        this.props.mMarkers(testMark)
     }
 
     /*
+    * Capitalize first letter in name.
+    * Courtesy of Stackoverflow: https://stackoverflow.com/questions/48387180/is-it-possible-to-capitalize-first-letter-of-text-string-in-react-native-how-to
+    */
+    
+    capitalize(name){
+        return name.charAt(0).toUpperCase() + name.slice(1)
+    }
+    
+    /*
     * Function to populate InfoWindow 
-    * Once the window is clicked. the window will close
-    *
-    * (same code in App.js -> attemp to use from App.js for menu)
+    * Click the X to close the window
     */
     populateInfoWindow = (marker, infoWindow) => {
-        // console.log(marker.title)
-        if(infoWindow.marker !== marker){
-            this.setState(infoWindow.marker = marker)
-            infoWindow.setContent(`
-            <div>   
-                <h3>${marker.title}</h3>
-            </div>
-            <div>
-                <ul style='list-style-type:none'>
-                    <li>${marker.address}</li>
-                    <li>${marker.city}</li>
-                    <li><a href='${marker.link}'>More Information</a></li>
-                </ul>
-            </div>
-            `)
-            infoWindow.open(this.map, marker)
-            infoWindow.addListener('click', function(){
-                infoWindow.marker = null
-            })
-        }
+        
+      if(infoWindow.marker !== marker){
+        this.setState(infoWindow.marker = marker)
+        infoWindow.setContent(`
+        <div>   
+            <h3 style='margin-bottom:0'>${this.capitalize(marker.title)}</h3>
+        </div>
+        <div>
+            <ul style='list-style-type:none; display:inline'>
+                <li>${marker.category}</li>
+                <li>${marker.city}</li>
+            </ul>
+        </div>
+        `)
+        infoWindow.open(this.map, marker)
+        infoWindow.addListener('close', () => {
+            infoWindow.marker = null
+        })
     }
 
     style = {
@@ -202,9 +139,7 @@ class Container extends Component{
     }
 
     render(){
-        // let {markers} = this.state
-        //  console.log(this.props.mLocations)
-        // console.log(this.state.markers)
+  
         return(
         <div>
             <div 
